c823ab7e0df377c7226a6a28b004d30bc19119a2
harisw <hari14@mhs.if.its.ac.id>
2022-05-31 07:12:13 +0900

    Add inventory services and connected to local database

---
+++ inventory-service/src/main/java/com/harisw/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,31 @@
+package com.harisw.inventoryservice;
+
+import com.harisw.inventoryservice.model.Inventory;
+import com.harisw.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_11");
+			inventory1.setQuantity(50);
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+}
